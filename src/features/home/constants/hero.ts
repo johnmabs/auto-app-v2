cd009ea81b10b741803e0958311147917d9fa23b
John Mabiala <johnmabiala@hotmail.com>
2026-06-25 08:45:47 +0100

    Update price of FEATURED_CAR in hero.ts

--- src/features/home/constants/hero.ts
+++ src/features/home/constants/hero.ts
@@ -14,7 +14,7 @@ export const TRUST_BADGES: TrustBadge[] = [
 
 export const FEATURED_CAR = {
   name: "Toyota RAV4 Hybrid",
-  price: "18\u00a0500\u00a0000 FCFA",
+  price: "3\u00a0500\u00a0000 FCFA",
   imageUrl:
     "https://res.cloudinary.com/dqasdhtwp/image/upload/q_auto/f_auto/v1780736450/featured-vhicle-toyota-hybrid_o8n5no.jpg",
   imageAlt: "Toyota RAV4 disponible à l'importation au Congo",
